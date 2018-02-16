18ee8eaf50a63c2ca983b3858a720af763c2f918
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2018-02-15 20:34:02 -0500

    Add files

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  Pusher
+//
+//  Created by Jeff Kereakoglow on 2/15/18.
+//  Copyright © 2018 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
