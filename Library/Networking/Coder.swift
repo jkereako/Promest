5e557f7c9ed89d20a67cfe26ca95fd3fd9a3005d
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2018-02-18 08:52:56 -0500

    Add comments

--- Library/Networking/Coder.swift
+++ Library/Networking/Coder.swift
@@ -9,12 +9,16 @@
 import Foundation
 import Promises
 
+// JSONEncoder and JSONDecoder perform their respective work on the the UI thread by default. As
+// such, we indicate to the Promise that we want this work to occur on a background thread.
 final class Coder: CoderType {
+
+    /// The dispatch queue for the background thread.
     private let dispatchQueue = DispatchQueue(label: "JSONCoderQueue")
 
     /// Asynchronously encodes a contract to JSON data.
     ///
-    /// - Parameter contract:
+    /// - Parameter contract: The struct from which JSON will be serialized
     /// - Returns: a Data instance of encoded JSON
     func encode<T: Encodable>(_ contract: T) -> Promise<Data> {
         return Promise<Data>(on: dispatchQueue) { fulfill, reject in
@@ -32,7 +36,7 @@ final class Coder: CoderType {
     ///
     /// - Parameters:
     ///   - data: The JSON data to decode
-    ///   - to: The contract type to decode to
+    ///   - to: The struct to which JSON will be deserialized
     /// - Returns: A contract
     func decode<T: Decodable>(_ data: Data, to: T.Type) -> Promise<T> {
         return Promise<T>(on: dispatchQueue) { fulfill, reject in
